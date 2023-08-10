28eb2076773d8314c7955a94d50ecd81483c9140
José Pereira Macedo <arseniumgx@gmail.com>
2023-08-10 18:37:59 -0300

    feat(aula1): criando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "styles/globals.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <h1>Olar</h1>
-      <hr />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/sobre-mim" element={<h1>Sobre mim...</h1>} />
+        <Route path="*" element={<h1>Not found...</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
